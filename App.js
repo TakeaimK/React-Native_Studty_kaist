184914e82f8ad2f68e4e508f5fca177f0a289e03
TakeaimK <44010902+TakeaimK@users.noreply.github.com>
2020-01-03 15:49:00 +0900

    add touch handling

--- App.js
+++ App.js
@@ -13,6 +13,11 @@ import {
   Text,
   Image,
   TextInput,
+  Button,
+  Alert,
+  TouchableHighlight,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
 } from 'react-native';
 
 import {
@@ -63,12 +68,18 @@ export default class App extends Component<Props>{
      super(props);
      this.state={text:""}
   }
+
+  _onPressButton(){
+    Alert.alert('you tapped the button!');
+  }
+  _onLongPressButton(){
+    Alert.alert('you long press the button!');
+  }
   render(){
     let pic = {
       uri : 'https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png'
     }
     return (
-      
         <View style={{
           flex:1, 
           justifyContent: 'center',
@@ -81,6 +92,37 @@ export default class App extends Component<Props>{
           <Text style={{padding: 10, fondSize:42}}>
             {this.state.text.split(' ').map((word)=> word&&'✨').join('')}
           </Text>
+          <View style={styles.button}>
+            <Button style={{margin:20, fontSize:30}}
+              onPress={()=>{
+              Alert.alert('you tapped the button!');
+            }}
+            title="일반 버튼"
+            color="black"
+            />
+          </View>
+          <TouchableHighlight
+            onPress={this._onPressButton}
+            underlayColor="white"
+            >
+              <View style={styles.touch}>
+                <Text style={styles.white}>터치 가능한 영역</Text>
+              </View>
+          </TouchableHighlight>
+          <TouchableOpacity
+             onPress={this._onPressButton}
+             >
+             <View style={styles.touch}>
+                <Text style={styles.white}>터치 반응이 있는 영역</Text>
+              </View>
+          </TouchableOpacity>
+          <TouchableWithoutFeedback
+             onPress={this._onPressButton}
+             >
+               <View style={styles.touch}>
+                <Text style={styles.white}>피드백 없는 터치 가능 영역</Text>
+              </View>
+             </TouchableWithoutFeedback>
         </View>
      
         
@@ -105,9 +147,23 @@ const styles = StyleSheet.create({
     fontWeight:'bold',
     fontSize:20,
   },
-  red:{
-    color:'red',
+  white:{
+    color:'white',
   },
+  button:{
+    margin:20,
+    fontSize:40,
+  },
+  touch:{
+    marginBottom:30,
+    width:260,
+    height:100,
+    alignItems:'center',
+    justifyContent:'center',
+    backgroundColor:'blue',
+    color:'white',
+  },
+  
 });
 
 
